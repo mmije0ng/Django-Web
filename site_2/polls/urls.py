90075d2ec4b2d7e3aa3ccbad3a01d5add8b3034a
mijeong <victon777@naver.com>
2024-06-28 18:26:33 +0900

    Feature: 메인페이지(설문조사) 생성

--- site_2/polls/urls.py
+++ site_2/polls/urls.py
@@ -4,4 +4,15 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'), # localhost:8000/polls/
     
+    # 설문조사 주제 상세 페이지
+    # localhost:8000/polls/1/
+    path('<int:question_id>/', views.detail, name='detail'), 
+    
+    # 설문조사 선택지별 투표 post 요청
+    # localhost:8000/polls/1/vote
+    path('<int:question_id>/vote/', views.vote, name='detail'),
+
+    # 설문조사 선택지별 투표 결과
+    # localhost:8000/polls/1/results
+    path('<int:question_id>/results/', views.results, name='results'),
 ]
--- site_2/polls/views.py
+++ site_2/polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # 메인페이지
+# 모든 설문조사 주제를 보여줌
 def index(request):
-    return HttpResponse('Hello World')
+    latest_question_list = Question.objects.order_by('pub_date') # 날짜기준 오름차순, -pub_date' 일시 내림차순
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+
+
+# 설문지별 상세페이지
+def detail(request, question_id): 
+
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+def vote(request, question_id):
+    response = "You're looking at vote of question {}."
+    return HttpResponse(response.format(question_id))
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}."
+    return HttpResponse(response.format(question_id))
