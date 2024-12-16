32869edd411f3daef2794e7a68ceef1621b48c1a
mmije0ng <victon777@naver.com>
2024-12-16 21:18:54 +0900

    Rename: 폴더 구조 변경

--- site_1/lotto/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from lotto.models import GuessNumbers # from .models import GuessNumbers
-
-# Register your models here.
-
-admin.site.register(GuessNumbers) # Model 클래스 등록
