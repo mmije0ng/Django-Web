3629aefd17558bc9e403331d4912aeff61e9a03b
mmije0ng <victon777@naver.com>
2025-01-03 14:58:05 +0900

    [#8] Feat: OpenCV 설치 & django project 및 app 생성

--- site_2/site_2/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # polls라는 url로 시작하면 앱 폴더의 polls.py와 연결
-    path('polls/', include('polls.urls')),
-]
