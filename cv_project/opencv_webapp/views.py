f82ae09860f5bc06d37960cad71d75b31144246b
mmije0ng <victon777@naver.com>
2025-01-08 16:29:57 +0900

    [#8] Fea: 이미지 파일 업로드 처리 및 DB 저장

--- cv_project/opencv_webapp/views.py
+++ cv_project/opencv_webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from .forms import SimpleUploadForm
+from .forms import SimpleUploadForm, ImageUploadForm
 from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 def first_view(request):
     return render(request, 'opencv_webapp/first_view.html', {})
@@ -28,3 +29,29 @@ def simple_upload(request):
         form = SimpleUploadForm()
         context = {'form': form} # empty form
         return render(request, 'opencv_webapp/simple_upload.html', context)
+
+def detect_face(request):
+    if request.method == 'POST' :
+        # 비어있는 Form에 사용자가 업로드한 데이터를 넣고 검증합니다.
+        form = ImageUploadForm(request.POST, request.FILES) # filled form
+        if form.is_valid():
+            post = form.save(commit=False) 
+            
+            # save() 함수는 DB에 저장될 ImageUploadModel 클래스 객체 자체를 리턴함 (== record 1건) 
+            # Form에 채워진 데이터를 DB에 실제로 저장하기 전에 변경/추가할 수 있음 (commit=False)
+            # ex) post.description = papago.translate(post.description)
+            post.save() # Form 객체('form')에 채워져 있는 데이터를 DB에 실제로 저장 
+
+            # document : ImageUploadModel Class에 선언되어 있는 “document”에 해당
+            imageURL = settings.MEDIA_URL + form.instance.document.name
+            # == form.instance.document.url
+            # == post.document.url
+            # == '/media/images/2021/10/29/ses_XQAftn4.jpg'
+            # print(form.instance, form.instance.document.name, form.instance.document.url)
+            # cv_detect_face(settings.MEDIA_ROOT_URL + imageURL) # 추후 구현 예정
+
+            return render(request, 'opencv_webapp/detect_face.html', {'form':form, 'post':post})
+
+    else:
+        form = ImageUploadForm() # empty form
+        return render(request, 'opencv_webapp/detect_face.html', {'form':form})
